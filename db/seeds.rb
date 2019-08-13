34cdb3d570307473182440d6690cfc874f7fb4c9
Ellen Lee <ellen811003@gmail.com>
2019-08-13 14:49:10 +0100

    completed wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,11 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  article = Article.new(
+    title:    Faker::Book.title,
+    content:  Faker::DrivingLicence.uk_driving_licence,
+  )
+  article.save
+end
